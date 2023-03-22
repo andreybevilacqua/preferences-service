eb5ca327a253b1afef6b2a282669dbab06df4432
vbevilacqua <bevilacqua.vinicius@gmail.com>
2023-03-22 16:01:52 +0100

    Fix controller

--- src/main/java/com/bevilacquas/preferencesservice/api/PreferencesController.java
+++ src/main/java/com/bevilacquas/preferencesservice/api/PreferencesController.java
@@ -9,12 +9,13 @@ import com.bevilacquas.preferencesservice.application.preference.PreferenceReque
 import com.bevilacquas.preferencesservice.application.preference.PreferenceResponse;
 import com.bevilacquas.preferencesservice.application.preference.commands.CreatePreferenceCommand;
 import com.bevilacquas.preferencesservice.application.preference.queries.GetAllPreferencesQuery;
+import com.bevilacquas.preferencesservice.application.preference.queries.GetPreferenceByIdQuery;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,8 +36,8 @@ public class PreferencesController implements Command<PreferenceResponse> {
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<List<PreferenceResponse>> getPreferenceById(@RequestAttribute UUID id) {
-    return new ResponseEntity<>(new GetAllPreferencesQuery().execute(pipeline), OK);
+  public ResponseEntity<PreferenceResponse> getPreferenceById(@PathVariable String id) {
+    return new ResponseEntity<>(new GetPreferenceByIdQuery(UUID.fromString(id)).execute(pipeline), OK);
   }
 
   @PostMapping
