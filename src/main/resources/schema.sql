ec54fb0064b3dc89d494e81b1758f2069362f88d
andrey.bevilacqua <andreybevilacqua@gmail.com>
2023-04-10 12:10:20 +0200

    Adding missing tables in schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,6 +3,16 @@ CREATE TABLE Preference (
     name varchar2(200)
 );
 
+CREATE TABLE Solution (
+    id UUID NOT NULL PRIMARY KEY,
+    name varchar2(200)
+);
+
+CREATE TABLE Universal_Preference (
+    id UUID NOT NULL PRIMARY KEY,
+    name varchar2(200)
+);
+
 CREATE TABLE UserApp (
     id UUID NOT NULL PRIMARY KEY,
     name varchar2(400)
