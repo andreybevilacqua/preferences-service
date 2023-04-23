b4bc68dd58b81f0c0dd72d8f3c545a4bf85bc851
vbevilacqua <bevilacqua.vinicius@gmail.com>
2023-04-23 17:55:30 +0200

    Change schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,19 +1,35 @@
-CREATE TABLE Preference (
-    id UUID NOT NULL PRIMARY KEY,
-    name varchar2(200)
-);
-
 CREATE TABLE Solution (
-    id UUID NOT NULL PRIMARY KEY,
-    name varchar2(200)
+    name VARCHAR(200) PRIMARY KEY,
+    type VARCHAR(200),
+    is_active boolean NOT NULL,
+    created_date DATETIME NOT NULL,
+    updated_date DATETIME NOT NULL
 );
 
 CREATE TABLE Universal_Preference (
-    id UUID NOT NULL PRIMARY KEY,
-    name varchar2(200)
+    name VARCHAR(200) PRIMARY KEY,
+    value VARCHAR(2000),
+    is_active boolean NOT NULL,
+    created_date DATETIME NOT NULL,
+    updated_date DATETIME NOT NULL
+);
+
+CREATE TABLE Solution_Preference (
+    universal_preference_name VARCHAR(200) PRIMARY KEY,
+    solution_name VARCHAR(200) PRIMARY KEY,
+    value VARCHAR(200),
+    is_active boolean NOT NULL
 );
 
 CREATE TABLE UserApp (
     id UUID NOT NULL PRIMARY KEY,
-    name varchar2(400)
+    name VARCHAR(400) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    created_date DATETIME NOT NULL
+);
+
+CREATE TABLE User_Preference (
+    id UUID NOT NULL PRIMARY KEY,
+    name VARCHAR(400),
+    created_date DATETIME NOT NULL
 );
